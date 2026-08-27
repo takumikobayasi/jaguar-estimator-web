23e07ad5b546ac72e1919e4d8fcbf929f68f241d
takumikobayasi <takumikobayasi@gmail.com>
2026-08-28 08:27:53 +0900

    Use latest Apps Script deployment

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.JUGGLER_API_URL = 'https://script.google.com/macros/s/AKfycbxXfv2JBbiLjMcH1RwLGISfDb7II1PRHC9pKisxydRfP1HOyrduPygMvH3-TaRdrLUAzA/exec';
+window.JUGGLER_API_URL = 'https://script.google.com/macros/s/AKfycbwPXKMKoxlJqyTcmnoWEEPaWtz59RUGmmsMiuhUdAH4rnNJ5ptxaYA8FJwO_qVjq-qVAA/exec';
