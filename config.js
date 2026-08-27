600a63886ee966b8676ceebcb8e3596074e29b21
takumikobayasi <takumikobayasi@gmail.com>
2026-08-28 07:15:28 +0900

    Point web v1.0.0 to the new Apps Script deployment

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.JUGGLER_API_URL = 'https://script.google.com/macros/s/AKfycbzWe-rjSvmgXCox1dAaMMeeFaWqfd65TAS-X090tJMfArlE4ZUdLhlwUf1BhFeidxWzSQ/exec';
+window.JUGGLER_API_URL = 'https://script.google.com/macros/s/AKfycbzrS_3MuesmdeoQvfg0Zm6LDXrRYGRJe_1ceZObc0y0zh_KYyTCuf3W9Q85xpBYoHVB/exec';
